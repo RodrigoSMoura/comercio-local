54ffa7f87f60e39c0d2e2cade0de15ec520bfeb4
Rodrigo Moura <rodrigosilveira.moura@gmail.com>
2020-04-25 15:29:40 -0300

    Inicio Frontend

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -2,8 +2,25 @@ import React from 'react';
 
 import './styles.css';
 
+import comercioItemsImg from '../../assets/comercioitens.png'
+import logoImg from '../../assets/comerciolocal_logo.svg'
+
 export default function Login(){
     return (
-        <h1>Hello</h1>
+        <div className="login-container">
+            <section className="form">
+                <img src={logoImg} alt="Comercio Local"/>
+
+                <form>
+                    <h1>Faça seu login</h1>
+                    <input placeholder="Sua Id"/>
+                    <button type="submit">Entrar</button>
+                    <a href="/cadastrar" >
+                        Não tenho cadastro
+                    </a>
+                </form>
+            </section>
+            <img src={comercioItemsImg} alt="Comercio" />
+        </div>
     );
 }
